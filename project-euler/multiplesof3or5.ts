48e7cfc210bf474572ea6d8538b25e147b8cfadf
LauraCastrillonMp <lauracastrillon.mp@gmail.com>
2024-02-09 21:09:25 -0500

    Exercise 1: Multiples of 3 or 5

---
+++ project-euler/multiplesof3or5.ts
@@ -0,0 +1,20 @@
+/*
+    If we list all the natural numbers below that are multiples of 3 or 5, we get and 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+function multiplesOf3or5(n: number): number {
+    let sum = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+console.log(multiplesOf3or5(10));
+console.log(multiplesOf3or5(1000));
